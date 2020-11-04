039c3a654f70c008d05c662873bf15d324dc45d6
panw3i <panw3i@gmail.com>
2020-11-04 13:57:01 +0800

    类型别名

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,28 @@
+<template>
+<ul>
+  <li v-for="item in list" :key="item.description" >{{item.description}}</li>
+</ul>
+</template>
+
+<script lang="ts">
+import {defineComponent,PropType} from "vue"
+
+export interface ColumnProps {
+  id:number;
+  title:string;
+  description:string;
+}
+export default defineComponent({
+  name:"ColumnList",
+  props:{
+    list:{
+      type:Array as PropType<ColumnProps[]>,  // 类型判言
+      required:true,
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,27 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <h1>{{ count }}</h1>
+  <h1>{{ double }}</h1>
+  <button @click="increase">+1</button>
 </template>
 
 <script>
+import {ref,computed} from "vue";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  setup(){
+    // ref 是一个函数，它接受一个参数，返回的就是一个神奇的 响应式对象 。我们初始化的这个 0 作为参数包裹到这个对象中去，在未来可以检测到改变并作出对应的相应。
+    const count = ref(0)
+    const double = computed(() => {
+      return count.value * 2
+    })
+    const increase = () => {
+      count.value++
+    }
+    return {
+      count,
+      increase,
+      double
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
